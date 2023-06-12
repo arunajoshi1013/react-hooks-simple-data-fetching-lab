965343eb9295a872b8ed82afd66b90cb8810bbba
Aruna Joshi <arunajoshi1013@gmail.com>
2023-06-12 12:37:00 -0700

    Dat fetching with usecase

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,33 @@
 // create your App component here
+import React, { useEffect, useState } from 'react';
+
+const App = () => {
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    const fetchDogImage = async () => {
+      try {
+        const response = await fetch('https://dog.ceo/api/breeds/image/random');
+        const data = await response.json();
+        setDogImage(data.message);
+      } catch (error) {
+        console.error('Error fetching dog image:', error);
+      }
+    };
+
+    fetchDogImage();
+  }, []);
+
+  return (
+    <div>
+      <h1>Random Dog Image</h1>
+      {dogImage ? (
+        <img src={dogImage} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default App;
